fee0d3b85a01bc5e883213dc0919ec518f447890
shubhamRev <shubhamyadav@revinfotech.com>
2024-02-06 23:51:53 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,11 +18,16 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 export default function App({ Component, pageProps }: AppProps) {
   const network = WalletAdapterNetwork.Mainnet;
 
-
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
- 
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
